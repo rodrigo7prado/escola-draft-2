b5deaab7180a9840d6d341ca350feda8ad0bdfe9
rodrigo7prado <rodrigo7prado@example.com>
2025-11-16 14:33:27 -0300

    fix: corrigindo modelo do prisma

---
+++ prisma/migrations/20251116090533_add_campos_dados_escolares/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "alunos" ADD COLUMN     "causaEncerramentoEscolar" VARCHAR(100),
+ADD COLUMN     "motivoEncerramento" VARCHAR(100),
+ADD COLUMN     "recebeOutroEspacoEscolar" VARCHAR(50),
+ADD COLUMN     "situacaoEscolar" VARCHAR(50);
